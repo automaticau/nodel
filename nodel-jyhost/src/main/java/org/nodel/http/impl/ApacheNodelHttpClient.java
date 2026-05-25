9e0b0b0f242e81585355c1a69cdd9d7238036827
Justin Parker <justin.parker@lumicom.com.au>
2026-05-25 17:07:02 +1000

    typo

--- nodel-jyhost/src/main/java/org/nodel/http/impl/ApacheNodelHttpClient.java
+++ nodel-jyhost/src/main/java/org/nodel/http/impl/ApacheNodelHttpClient.java
@@ -22,6 +22,7 @@ import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
+import org.apache.http.ProtocolException;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.Credentials;
 import org.apache.http.auth.NTCredentials;
